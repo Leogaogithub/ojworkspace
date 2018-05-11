2da619ec4ffe31e69a2cded14f2c76e6c10255e7
leo <longhaogao@gmail.com>
2018-05-11 00:41:05 -0700

    add L47PermutationsII

---
+++ src/leetcode/L47PermutationsII.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+/**
+ * Created by leo on 5/10/18.
+ */
+public class L47PermutationsII {
+
+}
---
+++ test/leetcode/L47PermutationsIITest.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class L47PermutationsIITest {
+
+    @Test
+    public void permuteUnique() throws Exception {
+        final L47PermutationsII solution = new L47PermutationsII();
+        final int[] input = {1, 1, 3};
+        final List<List<Integer>> output = solution.permuteUnique(input);
+        output.forEach(
+                m -> {
+                    m.forEach(
+                            n -> System.out.print(n));
+                    System.out.println("");
+                }
+        );
+    }
+}
