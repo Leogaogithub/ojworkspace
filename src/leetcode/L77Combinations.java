539b2d2d2a54fd3c71cfcd5ed8c901857aa74016
leo <longhaogao@gmail.com>
2018-05-12 17:22:43 -0700

    add solution2 for L77Combinations

--- src/leetcode/L77Combinations.java
+++ src/leetcode/L77Combinations.java
@@ -10,10 +10,13 @@ public class L77Combinations {
     public List<List<Integer>> combine(final int n, final int k) {
         result = new ArrayList<>();
         final List<Integer> curList = new ArrayList<>();
-        helper(k, 1, n, curList);
+        //solution1
+        //helper(k, 1, n, curList);
+        helper2(1, n, k, curList);
         return result;
     }
 
+    // solution1
     void helper(final int k, final int curValue, final int n, final List<Integer> curList) {
         if (k == curList.size()) {
             result.add(new ArrayList<>(curList));
@@ -30,5 +33,20 @@ public class L77Combinations {
 
         // not select
         helper(k, curValue + 1, n, curList);
+    }
+
+    // solution2 better then solution1
+    // solution2 have less function call stack
+    void helper2(final int start, final int n, final int k, final List<Integer> curList) {
+        if (k == 0) {
+            result.add(new ArrayList<>(curList));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            curList.add(i);
+            helper2(i + 1, n, k - 1, curList);
+            curList.remove(curList.size() - 1);
+        }
     }
 }
