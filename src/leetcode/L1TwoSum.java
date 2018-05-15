b06fb715532880e1c92d2720fa7bc2d91d89257d
leo <longhaogao@gmail.com>
2018-05-15 00:03:37 -0700

    L1TwoSum

---
+++ src/leetcode/L1TwoSum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L1TwoSum {
+
+    public int[] twoSum(final int[] nums, final int target) {
+        final Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
